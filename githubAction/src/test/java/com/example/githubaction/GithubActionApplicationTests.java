8b1976b84bf2d3d644838589e219b7e15754b973
kimjongha99 <jjhsun99@gmail.com>
2023-12-15 15:33:27 +0900

    asd

--- githubAction/src/test/java/com/example/githubaction/GithubActionApplicationTests.java
+++ githubAction/src/test/java/com/example/githubaction/GithubActionApplicationTests.java
@@ -3,13 +3,17 @@ package com.example.githubaction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
 
 @SpringBootTest
-class GithubActionApplicationTests {
+class GithubactionstestApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testFail(){
+        //throw new RuntimeException("실패했따");
     }
 
 
